b502000a69448d5955bc02a64843b919fe341600
N_Naru <deadee31@gmail.com>
2021-12-29 13:24:46 +0900

    Initial Commit

---
+++ KR-EN dic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KR-EN dic
+//
+//  Created by 엔나루 on 2021/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KR-EN dic/KR_EN_dicApp.swift
@@ -0,0 +1,17 @@
+//
+//  KR_EN_dicApp.swift
+//  KR-EN dic
+//
+//  Created by 엔나루 on 2021/12/29.
+//
+
+import SwiftUI
+
+@main
+struct KR_EN_dicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
